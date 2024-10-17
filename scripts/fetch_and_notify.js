ce8398793094843a8c3269430aee780505a82ad7
shige-56 <unagi014@gmail.com>
2024-10-18 03:42:08 +0900

    環境変数が渡されているか

--- scripts/fetch_and_notify.js
+++ scripts/fetch_and_notify.js
@@ -1,5 +1,15 @@
-console.log('APIキー:', process.env.YOUTUBE_API_KEY);
-console.log('ディスコード:', process.env.DISCORD_WEBHOOK_URL);
+if (!process.env.YOUTUBE_API_KEY) {
+    console.error('YOUTUBE_API_KEY が見つかりません。');
+  } else {
+    console.log('YOUTUBE_API_KEY:', process.env.YOUTUBE_API_KEY);
+  }
+  
+  if (!process.env.DISCORD_WEBHOOK_URL) {
+    console.error('DISCORD_WEBHOOK_URL が見つかりません。');
+  } else {
+    console.log('DISCORD_WEBHOOK_URL:', process.env.DISCORD_WEBHOOK_URL);
+  }
+  
 
 const { fetchVideos } = require('../backend/youtube-api');
 const { sendNotification } = require('../backend/discord-notify');
